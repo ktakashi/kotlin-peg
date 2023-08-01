9b944fe07759d4d3e5248c54c4556db216d247a8
Takashi Kato <ktakashi@ymail.com>
2023-08-01 11:04:20 +0200

    Fixing sed command

--- parser-combinator/build.gradle.kts
+++ parser-combinator/build.gradle.kts
@@ -82,10 +82,11 @@ publishing {
     }
     repositories {
         maven {
-            name = "sonatypeRepository"
             val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            val isSnapshot = version.toString().endsWith("SNAPSHOT")
+            url = if (isSnapshot) snapshotsRepoUrl else releasesRepoUrl
+            name = if (isSnapshot) "sonatypeSnapshotRepository" else "sonatypeRepository"
             credentials {
                 if (hasProperty("ossrhUsername")) {
                     username = property("ossrhUsername") as String
